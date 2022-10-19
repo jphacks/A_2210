44f644636bea4bacd4289677370ba95586b79375
mnt3710 <tomato3710@gmail.com>
2022-10-19 20:14:53 +0900

    home.dart 編集

--- frontend/lib/page/home.dart
+++ frontend/lib/page/home.dart
@@ -1,7 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/main.dart';
 
 Widget HomeContent(
-  String content,
+  String text,
+  List dataList,
 ) {
-  return (Text(content));
+  List ingredientNameList = ["にんじん"];
+  String? isSelectedItem = "にんじん";
+
+  for (var i = 0; i < dataList.length; i++)
+    ingredientNameList.add(dataList[i]["fields"]["Name"].toString());
+  return Scaffold(
+    body: SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          /*カテゴリボタン */
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              ElevatedButton(
+                //調理不可ボタン
+                onPressed: () {},
+                child: Text(
+                  "調理不可",
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                ),
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                ),
+              ),
+              SizedBox(
+                //ボタンの間隔
+                width: 10,
+              ),
+              ElevatedButton(
+                //調理可ボタン
+                onPressed: () {},
+                child: Text(
+                  "調理可",
+                  style: TextStyle(
+                    color: Colors.blue,
+                  ),
+                ),
+
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Colors.white),
+                ),
+              ),
+              SizedBox(
+                width: 10,
+              ),
+            ],
+          ),
+          for (var i = 0; i < 13; i++)
+            Card(
+              child: Container(
+                height: 100,
+                color: Colors.blue,
+              ),
+            ),
+          // Container(
+          //   width: 400,
+          //   height: 100,
+          //   decoration: BoxDecoration(
+          //     color: Colors.grey[100],
+          //     borderRadius: BorderRadius.circular(20),
+          //   ),
+          // ),
+        ],
+      ),
+    ),
+  );
+  /* DropdownButton(items: [DropdownMenuItem(child: "child" ,value: "hoge")], onChanged: onChanged)); */
 }
