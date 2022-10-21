fee00caacf158e1ff4e68f24b4b65b3030de0c97
mnt3710 <tomato3710@gmail.com>
2022-10-22 02:44:05 +0900

    add_conditonsのconflict解消

--- frontend/lib/page/recipe/add_conditions.dart
+++ frontend/lib/page/recipe/add_conditions.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
 class AddConditions extends StatefulWidget {
   const AddConditions({super.key});
 
@@ -29,19 +28,6 @@ class _AddConditions extends State<AddConditions> {
                 primary: Colors.grey,
               ),
               onPressed: () {},
-=======
-
-Widget AddConditions(String content) {
-  return Column(
-    children: [
-      Container(
-        alignment: Alignment.centerRight,
-        child: ElevatedButton(
-          child: Text(
-            '取り消す',
-            style: TextStyle(
-              color: Colors.white,
->>>>>>> haratomo/20221021-1
             ),
           ),
           Container(
@@ -254,99 +240,6 @@ Widget AddConditions(String content) {
               },
             ),
           )
-<<<<<<< HEAD
         ]));
   }
 }
-=======
-        ),
-      ),
-      Row(
-        children: [
-          ConditionContainer('カニ'),
-          SizedBox(width: 20.0),
-          ConditionContainer('たまご'),
-          SizedBox(width: 20.0),
-          ConditionContainer('小麦'),
-        ],
-      ),
-      Spacer(flex: 1),
-      Container(
-        alignment: Alignment.centerLeft,
-        child: Text(
-          '時間',
-          style: TextStyle(
-            fontSize: 20,
-          )
-        ),
-      ),
-      Row(
-        children: [
-          ConditionContainer('朝食'),
-          SizedBox(width: 20.0),
-          ConditionContainer('昼食'),
-          SizedBox(width: 20.0),
-          ConditionContainer('夜食'),
-          SizedBox(width: 20.0),
-          ConditionContainer('間食'),
-        ],
-      ),
-      Spacer(flex: 1),
-      Container(
-        alignment: Alignment.centerLeft,
-        child: Text(
-          '分野',
-          style: TextStyle(
-            fontSize: 20,
-          )
-        ),
-      ),
-      Row(
-        children: [
-          ConditionContainer('主菜'),
-          SizedBox(width:20.0),
-          ConditionContainer('副菜'),
-          SizedBox(width: 20.0),
-          ConditionContainer('汁物'),
-        ],
-      ),
-      Spacer(flex: 2),
-      Container(
-        alignment: Alignment.bottomCenter,
-        child: ElevatedButton(
-          child: Text(
-            '条件の追加',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-          style: ElevatedButton.styleFrom(
-            primary: Colors.grey,
-          ),
-          onPressed: () {},
-        ),
-      )
-    ]
-  );
-}
-
-Widget ConditionContainer (String condition) {
-  return Container(
-    width: 100,
-    height: 100,
-    child: ElevatedButton(
-      child: Text(
-        condition,
-        style: TextStyle(
-          fontSize: 20,
-          color: Colors.black,
-        )
-      ),
-      onPressed: (){},
-      style: ElevatedButton.styleFrom(
-        primary: Colors.white,
-      ),
-    ),
-  );
-}
->>>>>>> haratomo/20221021-1
