c691c3179c2920a3dc12fc3c7a101f2f98297fae
mnt3710 <tomato3710@gmail.com>
2022-10-22 08:35:12 +0900

    url取得完了

---
+++ frontend/lib/page/meal_prep_list/rakuten_api.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class RakutenApi extends StatefulWidget {
+  const RakutenApi({super.key});
+
+  @override
+  State<RakutenApi> createState() => _RakutenApi();
+}
+
+class _RakutenApi extends State<RakutenApi> {
+  String recipeUrl = "";
+
+  void fetchRecipe() async {
+    final dio = Dio();
+    final id = dotenv.get("RAKUTEN_APPLICATION_ID");
+    final parm =
+        'format=json&categoryType=small&elements=categoryName%2CcategoryUrl&applicationId=${id}';
+    final url =
+        'https://app.rakuten.co.jp/services/api/Recipe/CategoryList/20170426?${parm}';
+    final response = await dio.get(url);
+    final target = "じゃがいも";
+
+    if (response.statusCode == 200) {
+      try {
+        final data = response.data["result"]["small"];
+        for (var i = 0; i < data.length; i++) {
+          if (data[i]["categoryName"] == target) {
+            setState(() {
+              recipeUrl = data[i]["categoryUrl"];
+            });
+          }
+        }
+        ;
+        print(recipeUrl);
+      } catch (e) {
+        throw e;
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("レシピ詳細")),
+      body: Column(children: [
+        Text(recipeUrl.toString()),
+        OutlinedButton(onPressed: fetchRecipe, child: Text("api"))
+      ]),
+    );
+  }
+}
